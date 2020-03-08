b728d69cfc139c001757c7acff37459ff91aef1f
armelo483 <armelo483@users.noreply.github.com>
2020-03-08 16:50:58 +0100

    _snackBarWidgets_08-03-2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(SkyAppStatelessWidget());
 
-class MyApp extends StatelessWidget {
+class SkyAppStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sky App',
+      title: 'Usage du snackBar',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome hhh'),
+          title: Text('Usage du widget SnackBar'),
         ),
-        body: Center(
-          child: Text('World'),
-        ),
+        body: SnackBarPage(),
+      ),
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('SnackBar equivalent de modale'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+
+          // Find the Scaffold in the widget tree and use
+          // it to show a SnackBar.
+          //Equivalent de document.getElementById() en JS mais juste pour les snackBar
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Show SnackBar'),
       ),
     );
   }
